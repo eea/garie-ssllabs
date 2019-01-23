7fe9f7c7758877dd615acd1fbde958a162ac8f21
valentinab25 <valentinab25>
2019-01-23 11:45:42 +0200

    Allow insert of zero

--- src/influx/index.js
+++ src/influx/index.js
@@ -27,7 +27,7 @@ const init = async () => {
 const saveData = async (url, data) => {
     try {
         const points = Object.keys(data).reduce((points, key) => {
-            if (data[key]) {
+            if (data[key] !=null ) {
                 points.push({
                     measurement: key,
                     tags: { url },
