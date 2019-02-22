4056506fd39c8237c3124636b20fc8b30d5b62e0
valentinab25 <valentinab25>
2019-02-22 19:47:10 +0200

    Randomise sleep

--- src/ssllabs.sh
+++ src/ssllabs.sh
@@ -12,10 +12,10 @@ mkdir -p $report_location
 timeout 1800 docker run --read-only --cap-drop all --rm jumanjiman/ssllabs-scan:latest -grade $1 > $report_location/ssllabs.txt 2>&1
 
 if [ $(grep "Assessment complete:"  $report_location/ssllabs.txt | wc -l ) -eq 0 ]; then
-	echo "There was a problem with docker:"
+	echo "There was a problem with docker for $1:"
 	cat $report_location/ssllabs.txt
-	echo "Will retry in 20 seconds"
-	sleep 20
+	echo "Will retry, after short pause"
+	sleep $(shuf -i 10-50 -n 1)
 	timeout 1800 docker run --read-only --cap-drop all --rm jumanjiman/ssllabs-scan:latest -grade $1 > $report_location/ssllabs.txt 2>&1
 fi
 
